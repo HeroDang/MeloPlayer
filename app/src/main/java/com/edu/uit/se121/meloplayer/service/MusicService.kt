e0ceac377e22e9d1c5aace998b979560f7acb9e7
DangHung <danghungabt.bte@gmail.com>
2023-04-07 22:52:52 +0700

    Customize Notification

--- app/src/main/java/com/edu/uit/se121/meloplayer/service/MusicService.kt
+++ app/src/main/java/com/edu/uit/se121/meloplayer/service/MusicService.kt
@@ -32,7 +32,7 @@ class MusicService : Service() {
         val notification = NotificationCompat.Builder(baseContext, ApplicationClass.CHANNEL_ID)
             .setContentTitle(PlayerActivity.musicListPA[PlayerActivity.songPosition].title)
             .setContentText(PlayerActivity.musicListPA[PlayerActivity.songPosition].artist)
-            .setSmallIcon(R.drawable.playlist_icon)
+            .setSmallIcon(R.drawable.music_icon)
             .setLargeIcon(
                 BitmapFactory.decodeResource(
                     resources,
