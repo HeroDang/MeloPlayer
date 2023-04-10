dae6c278dabca309d5ad503a9a2450aa9ee126b0
DangHung <danghungabt.bte@gmail.com>
2023-04-10 16:51:41 +0700

    Delete Playlist & Creating PlaylistDetails Class

--- app/src/main/java/com/edu/uit/se121/meloplayer/adapter/PlaylistViewAdapter.kt
+++ app/src/main/java/com/edu/uit/se121/meloplayer/adapter/PlaylistViewAdapter.kt
@@ -2,12 +2,19 @@ package com.edu.uit.se121.meloplayer.adapter
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.edu.uit.se121.meloplayer.PlaylistActivity
+import com.edu.uit.se121.meloplayer.PlaylistDetailsActivity
 import com.edu.uit.se121.meloplayer.databinding.PlaylistViewBinding
 import com.edu.uit.se121.meloplayer.model.Playlist
+import com.edu.uit.se121.meloplayer.model.exitApplication
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class PlaylistViewAdapter(private val context: Context, private var playlistList: ArrayList<Playlist>) :
     RecyclerView.Adapter<PlaylistViewAdapter.MyHolder>() {
@@ -15,6 +22,7 @@ class PlaylistViewAdapter(private val context: Context, private var playlistList
         val image = binding.playlistImg
         val name = binding.playlistName
         val root = binding.root
+        val delete = binding.playlistDeleteBtn
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
@@ -28,6 +36,29 @@ class PlaylistViewAdapter(private val context: Context, private var playlistList
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
         holder.name.text = playlistList[position].name
         holder.name.isSelected = true
+        holder.delete.setOnClickListener {
+            val builder = MaterialAlertDialogBuilder(context)
+            builder.setTitle(playlistList[position].name)
+                .setMessage("Do you want to delete playlist?")
+                .setPositiveButton("Yes") { dialog, _ ->
+                    PlaylistActivity.musicPlaylist.ref.removeAt(position)
+                    refreshPlaylist()
+                    dialog.dismiss()
+                }
+                .setNegativeButton("No") { dialog, _ ->
+                    dialog.dismiss()
+                }
+            val customDialog = builder.create()
+            customDialog.show()
+            customDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED)
+            customDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.RED)
+        }
+
+        holder.root.setOnClickListener {
+            val intent = Intent(context, PlaylistDetailsActivity::class.java)
+            intent.putExtra("index", position)
+            ContextCompat.startActivity(context, intent, null)
+        }
     }
 
     @SuppressLint("NotifyDataSetChanged")
